f73187b43a8d4c95a0b56770a5bc87d4a6c5a0a2
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-07-06 02:08:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const path = ["Cloud", "Desktop", "Video Sport", "Mobile", "Web"];
+const ui = document.getElementById("devs");
+
+path.forEach(path=>{
+	const li = document.createElement("li");
+	li.textContent = path + "development";
+	ui.appendChild(li);
+});
