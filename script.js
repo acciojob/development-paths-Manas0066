aec83c5638a8d6cb77337cf766cff488b6d7b841
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-07-06 02:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const ui = document.getElementById("devs");
 
 path.forEach(path=>{
 	const li = document.createElement("li");
-	li.textContent = path + "development";
+	li.textContent = path + " development";
 	ui.appendChild(li);
 });
